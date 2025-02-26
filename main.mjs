2f7faf01d9599e73a86881bba7af0af9fa0ca0c4
JanayAlam <janayalam820@gmail.com>
2025-02-27 05:18:36 +0600

    feat: added printPyramid function

--- main.mjs
+++ main.mjs
@@ -1,1 +1,9 @@
-console.log("This is initial work");
+function printPyramid(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let str = " ".repeat(rows - i);
+    let str2 = "*".repeat(i * 2 - 1);
+    console.log(str + str2 + str);
+  }
+}
+
+printPyramid(5);
